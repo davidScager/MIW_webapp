99be27aa5f09d7adefc9fb4ba25197a352240a69
Niek Mol <niekmol1994@gmail.com>
2021-04-23 14:05:44 +0200

    nu met security

---
+++ src/test/java/com/example/cryptobank/CryptoBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cryptobank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
