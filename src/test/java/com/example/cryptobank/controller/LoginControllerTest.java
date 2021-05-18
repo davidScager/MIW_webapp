8c06bdaeaf3b1164baa24ce5b29c3d222ca5585a
HvS <huibvanstraten@gmail.com>
2021-05-18 12:31:56 +0200

    mockMvc en DAOtesting. Work in progress. Tussentijdse commit. Nu verder met andere taak.

--- src/test/java/com/example/cryptobank/controller/LoginControllerTest.java
+++ src/test/java/com/example/cryptobank/controller/LoginControllerTest.java
@@ -11,8 +11,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  @Auth HvS
@@ -22,12 +28,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class LoginControllerTest {
 
     private MockMvc mockMvc;
-    private static LoginController loginController;
 
     @MockBean
     private static UserService mockUserService = Mockito.mock(UserService.class);
     @MockBean
     private static TokenService mockTokenService = Mockito.mock(TokenService.class);
+
+    public LoginController loginController= new LoginController(mockUserService, mockTokenService);
 
     @Autowired
     public LoginControllerTest(MockMvc mockMvc) {
@@ -39,7 +46,7 @@ class LoginControllerTest {
         Mockito.when(mockUserService.verifyUser("huib", "biuh")).thenReturn(
                 new User(13167, "huib", "van", "Straten", "29-01-1982", "van lierdreef", "huibvanstraten@gmail.com", "huib"));
         Mockito.when(mockTokenService.getToken()).thenReturn("DitIsEenToken");
-        loginController = new LoginController(mockUserService, mockTokenService);
+
     }
 
 //    @Test //test is TDD geschreven. Na slagen test is methode veranderd.
@@ -74,4 +81,25 @@ class LoginControllerTest {
 //        assertTrue(loginController.loginUser("huib", "biuh").getHeaders(). // sth ..);
 //    }
 
+    @Test
+    void loginTest() {
+        try {
+
+            MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post("/login");
+            request.param("username", "huib");
+            request.param("password", "biuh");
+
+            // stuur die request naar de server en vang de response op
+            ResultActions response = mockMvc.perform(request);
+
+            // test of de response aan de verwachtingen voldoet
+            response
+                    .andExpect(status().isOk())
+                    .andExpect(header().exists("authorization"))
+                    .andExpect(header().string("authorization", "987654"))
+                    .andDo(print());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
