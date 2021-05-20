218341dc685f6953829e9fca4d9cce5439eebef1
HvS <huibvanstraten@gmail.com>
2021-05-20 14:03:43 +0200

    password resetten met tokens parsen

--- src/main/java/com/example/cryptobank/service/TokenService.java
+++ src/main/java/com/example/cryptobank/service/TokenService.java
@@ -4,6 +4,8 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.sql.Date;
@@ -17,7 +19,7 @@ import java.util.Base64;
 
 @Service
 public class TokenService {
-
+    private Logger logger = LoggerFactory.getLogger(TokenService.class);
     private Instant now = Instant.now();
     private byte[] secret = Base64.getDecoder().decode("wf397i3th324m94j32832miuvuiububzduvabiovasrvnesonoe7632bfvn");
 
@@ -25,28 +27,28 @@ public class TokenService {
         super();
     }
 
-    public String generateJwtToken(String subject, int minutesValid) {
+    public String generateJwtToken(String email, String subject, int minutesValid) {
         String jwt = Jwts.builder()
                 .setSubject(subject)
+                .setId(email)
                 .setIssuedAt(Date.from(now))
                 .setExpiration(Date.from(now.plus(minutesValid, ChronoUnit.MINUTES)))
                 .signWith(Keys.hmacShaKeyFor(secret))
                 .compact();
+        logger.info(jwt);
         return jwt;
     }
 
 
-
-
-
-
-    public void parseToken(String token) {
+    public String parseToken(String token, String subject) {
 
         Jws<Claims> result = Jwts.parserBuilder()
-                .requireAudience("")
+                .requireSubject(subject)
                 .setAllowedClockSkewSeconds(10)
                 .setSigningKey(Keys.hmacShaKeyFor(secret))
                 .build().parseClaimsJws(token);
-
+        String id = result.getBody().getId();
+        logger.info(id);
+        return id;
     }
 }
