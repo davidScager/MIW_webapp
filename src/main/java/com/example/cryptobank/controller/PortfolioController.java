33c96f81fadf673156ff34978de9e5f0b84e8701
hille@Dorresteijn.nl <yung whateve>
2021-06-05 18:18:49 +0200

    Hallelujah

--- src/main/java/com/example/cryptobank/controller/PortfolioController.java
+++ src/main/java/com/example/cryptobank/controller/PortfolioController.java
@@ -1,14 +1,26 @@
 package com.example.cryptobank.controller;
 
+import com.example.cryptobank.domain.Actor;
+import com.example.cryptobank.domain.Asset;
+import com.example.cryptobank.domain.AssetPortfolioView;
+import com.example.cryptobank.domain.Portfolio;
+import com.example.cryptobank.repository.daointerfaces.AssetPortfolioDao;
+import com.example.cryptobank.repository.daointerfaces.PortfolioDao;
+import com.example.cryptobank.repository.jdbcklasses.JdbcActorDao;
+import com.example.cryptobank.service.assetenportfolio.ActorService;
 import com.example.cryptobank.service.assetenportfolio.PortfolioService;
+import com.example.cryptobank.service.transaction.TransactionService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class PortfolioController {
@@ -16,16 +28,21 @@ public class PortfolioController {
     private final Logger logger = LoggerFactory.getLogger(AssetController.class);
 
     private final PortfolioService portfolioService;
+    private final PortfolioDao portfolioDao;
+    private final AssetPortfolioDao assetPortfolioDao;
 
     @Autowired
-    public PortfolioController(PortfolioService portfolioService) {
+    public PortfolioController(PortfolioService portfolioService, PortfolioDao portfolioDao,
+                               ActorService actorService, AssetPortfolioDao assetPortfolioDao) {
         super();
         this.portfolioService = portfolioService;
+        this.portfolioDao = portfolioDao;
+        this.assetPortfolioDao = assetPortfolioDao;
         logger.info("New PortfolioController");
     }
 
     @GetMapping("/portfoliooverview")
-    public List<String> portfolioOverviewHandler(@RequestParam int userId) {
+    public List<String> portfolioOverviewHandler(@RequestParam int userId) throws JsonProcessingException {
         return portfolioService.showAssetOverview(userId);
     }
 
@@ -33,4 +50,18 @@ public class PortfolioController {
     public String portfolioValueHandler(@RequestParam int userId) {
         return portfolioService.showValueOfPortfolio(userId);
     }
+
+    @PostMapping("/listportfolio")
+    @CrossOrigin
+    public ResponseEntity<List<AssetPortfolioView>> listPortFolio(@RequestParam("token") String token) {
+        //TODO get token
+        System.out.println("fake token "+token);
+        int userId = 1;
+        int portfolioId = portfolioDao.getPortfolioIdByUserId((int)userId).getPortfolioId();
+        Portfolio portfolio = portfolioService.getByActor(userId);
+        System.out.println("portfolio "+ portfolio);
+        List<AssetPortfolioView> assetPortfolioView = assetPortfolioDao.getOverviewWithAmount(userId);
+        return new ResponseEntity<>(assetPortfolioView, HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/example/cryptobank/service/assetenportfolio/PortfolioService.java
+++ src/main/java/com/example/cryptobank/service/assetenportfolio/PortfolioService.java
@@ -1,38 +1,57 @@
 package com.example.cryptobank.service.assetenportfolio;
 
+import com.example.cryptobank.domain.Asset;
 import com.example.cryptobank.domain.Portfolio;
+import com.example.cryptobank.domain.Transaction;
+import com.example.cryptobank.repository.daointerfaces.PortfolioDao;
 import com.example.cryptobank.repository.jdbcklasses.RootRepository;
 import com.example.cryptobank.service.assetenportfolio.AssetService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class PortfolioService {
 
     private final RootRepository rootRepository;
+    private final PortfolioDao portfolioDao;
 
     private final Logger logger = LoggerFactory.getLogger(AssetService.class);
 
     @Autowired
-    public PortfolioService(RootRepository rootReposistory) {
+    public PortfolioService(RootRepository rootReposistory, PortfolioDao portfolioDao) {
         super();
         this.rootRepository = rootReposistory;
+        this.portfolioDao = portfolioDao;
         logger.info("New PortfolioService");
     }
 
-    public List<String> showAssetOverview(int userId) {
+    public List<String> showAssetOverview(int userId) throws JsonProcessingException {
         Portfolio portfolio = rootRepository.getPortfolioIdByUserId(userId);
-        List<String> overviewPortfolioList = rootRepository.showPortfolioOverview(portfolio.getPortfolioId());
-        return overviewPortfolioList;
+        List<Asset> overviewPortfolioList = rootRepository.showPortfolioOverview(portfolio.getPortfolioId());
+        List<String> portfolioPositionsAsJsonString = new ArrayList<>();
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString;
+        for (Asset asset :overviewPortfolioList) {
+            jsonString = mapper.writeValueAsString(asset);
+            portfolioPositionsAsJsonString.add(jsonString);
+        }
+        return portfolioPositionsAsJsonString;
     }
 
     public String showValueOfPortfolio(int userId) {
         Portfolio portfolio = rootRepository.getPortfolioIdByUserId(userId);
         String portfolioValueString = rootRepository.showPortfolioValue(portfolio.getPortfolioId());
         return portfolioValueString;
+    }
+
+    public Portfolio getByActor(int actor) {
+        return portfolioDao.getPortfolioIdByUserId(actor);
     }
 }
