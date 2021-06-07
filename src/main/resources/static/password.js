6d1b14d19a3773a5306ba867b8d9f3cfea3ebf58
HvS <huibvanstraten@gmail.com>
2021-06-07 12:58:20 +0200

    password.js een klasse gemaakt. data doorsturen naar backend lukt

--- src/main/resources/static/password.js
+++ src/main/resources/static/password.js
@@ -1,47 +1,63 @@
-function resetPassword() {
-    let mailRegex = new RegExp(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i);
-    let backEndPoint = "http://localhost:8080/reset/resetpassword";
-    let websiteRedirect = "http://localhost:8080/confirmed.html";
-    let arrayOfInserts = ["email"];
-
-    let dataHasWrongInput = false;
-    for (var i=0; i < arrayOfInserts.length; i++) {
-       dataHasWrongInput = (document.querySelector('#' + arrayOfInserts[i]).addEventListener('focusout', checkInserts(arrayOfInserts[i], mailRegex, dataHasWrongInput)));
-    }
-
-    if (dataHasWrongInput === false) {
-        sendData(insertToCheck, backEndPoint, websiteRedirect);
-    }
-
+function initiateReset() {
+    const password = new Password(new RegExp(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i),
+        "http://localhost:8080/reset/resetpassword", "http://localhost:8080/confirmed.html", ["email"]
+    )
+    password.resetPassword()
 }
 
+class Password {
+    dataHasWrongInput
+    mailRegex
+    backEndPoint
+    websiteRedirect
+    arrayOfInserts
+
+    constructor(mailRegex, backEndPoint, websiteRedirect, arrayOfInserts) {
+
+        this.mailRegex = mailRegex;
+        this.backEndPoint = backEndPoint;
+        this.websiteRedirect = websiteRedirect;
+        this.arrayOfInserts = arrayOfInserts;
+        this.dataHasWrongInput = false;
+    }
+
+resetPassword() {
+
+    for (var i=0; i < this.arrayOfInserts.length; i++) {
+       document.querySelector('#' + this.arrayOfInserts[i]).addEventListener('focusout', this.checkInserts(this.arrayOfInserts[i]));
+       console.log(this.dataHasWrongInput)
+    }
+
+    if (this.dataHasWrongInput === false) {
+        this.sendData(document.querySelector('#'+(this.arrayOfInserts)[0]).value, this.backEndPoint, this.websiteRedirect);
+    }
+}
 
 //algemene functie om velden in formulieren te checken op legitimiteit
-function checkInserts(insert, regex) {
+checkInserts(insert) {
     let insertToCheck = document.querySelector('#'+insert).value
-    console.log('insert is valide: ' + regex.test(insertToCheck)+ insertToCheck);
-    if (!regex.test(insertToCheck)) {
-        //warning
-        return true;
+    console.log('insert is valide: ' + this.mailRegex.test(insertToCheck)+ insertToCheck);
+    if (!this.mailRegex.test(insertToCheck)) {
+        this.dataHasWrongInput = true;
     }
 }
 
-function sendData(dataForBackEnd, backEndPoint, websiteRedirect) {
-    let formData = `${dataForBackEnd}`
-    fetch(backEndPoint, {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formData)  // moet worden omgezet naar een string
-    })
-        .then(response => {
-            console.log(response)
-            return response.json()
+sendData(dataForBackEnd) {
+        fetch(this.backEndPoint, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({email: dataForBackEnd})
         })
-        .then(window.location.replace(websiteRedirect))
-        .catch((error) => {
-            console.error('Foutje', error);
-        });
+            .then(response => {
+                console.log(response)
+                return response.json()
+            })
+            .then(window.location.replace(this.websiteRedirect))
+            .catch((error) => {
+                console.error('Foutje', error);
+            });
+    }
 }
