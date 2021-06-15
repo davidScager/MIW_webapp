5b7cf7460ea1a50892134d41e2496734ffb723c5
HvS <huibvanstraten@gmail.com>
2021-06-15 17:14:06 +0200

    TransactionServiceTest geschreven

--- src/test/java/com/example/cryptobank/service/transaction/TransactionServiceTest.java
+++ src/test/java/com/example/cryptobank/service/transaction/TransactionServiceTest.java
@@ -1,10 +1,11 @@
 package com.example.cryptobank.service.transaction;
 
+import com.example.cryptobank.domain.transaction.Transaction;
+import com.example.cryptobank.domain.transaction.TransactionData;
 import com.example.cryptobank.domain.transaction.TransactionHTMLClient;
 import com.example.cryptobank.domain.transaction.TransactionLog;
 import com.example.cryptobank.repository.jdbcklasses.RootRepository;
-import com.example.cryptobank.service.mailSender.GenerateMailContent;
-import com.example.cryptobank.service.mailSender.MailSenderService;
+import com.example.cryptobank.service.mailSender.mailsenderfacade.MailSenderFacade;
 import com.example.cryptobank.service.security.TokenService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,45 +13,48 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 
 import java.util.ArrayList;
+import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class TransactionServiceTest {
+    private TransactionHTMLClient testTransActionClient;
+    private TransactionLog expectedLog;
+    private TransactionData transactionData;
 
     @Mock
     private RootRepository mockRootRepository;
     @Mock
     private TokenService mockTokenService;
     @Mock
-    private MailSenderService mockMailSenderService;
-    @Mock
-    private GenerateMailContent mockGenerateMailContent;
+    private MailSenderFacade mockMailSenderFacade;
 
     @BeforeEach
     public void setUp() {
-        TransactionHTMLClient testTransActionClient = new TransactionHTMLClient("a", 12.0, "ab", 11.0);
+        testTransActionClient = new TransactionHTMLClient("a", 12.0, "ab", 11.0);
         ArrayList<TransactionHTMLClient> htmlClients = new ArrayList<>();
         htmlClients.add(testTransActionClient);
-        TransactionLog expectedLog = new TransactionLog(2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1);
+        expectedLog = new TransactionLog(2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1);
         mockRootRepository = Mockito.mock(RootRepository.class);
         mockTokenService = Mockito.mock(TokenService.class);
-        mockMailSenderService = Mockito.mock(MailSenderService.class);
-        mockGenerateMailContent = Mockito.mock(GenerateMailContent.class);
+        mockMailSenderFacade = Mockito.mock(MailSenderFacade.class);
+        transactionData = new TransactionData(1, 2, 2.0, "BTC", "ETH", 30000.0, "huibEnNiek");
+        transactionData.setTransactionCost(1);
         Mockito.when(mockTokenService.parseToken("123", "session")).thenReturn("huibEnNiek");
         Mockito.when(mockRootRepository.clientListForTransactionHTML("huibEnNiek")).thenReturn(htmlClients);
-        Mockito.when(mockRootRepository.createNewTransactionLog("BTC", "ETH", 2, 0.3)).thenReturn(expectedLog);
+        Mockito.when(mockRootRepository.createNewTransactionLog("BTC", "ETH", 2.0, 1)).thenReturn(expectedLog);
     }
 
     public TransactionServiceTest() {
         super();
     }
 
-//    @Test
-//    void returns_null_if_token_is_invalid() {
-//        var transactionTest = new TransactionService(mockRootRepository, mockTokenService, mockMailSenderService, mockGenerateMailContent, mailSenderFacade);
-//        List<TransactionHTMLClient> actualList = transactionTest.authorizeAndGetAssets("456");
-//        assertThat(actualList).isNull();
-//    }
+    @Test
+    void returns_null_if_token_is_invalid() {
+        var transactionTest = new TransactionService(mockRootRepository, mockTokenService, mockMailSenderFacade);
+        List<TransactionHTMLClient> actualList = transactionTest.authorizeAndGetAssets("456");
+        assertThat(actualList).isEmpty();
+    }
 
     @Test
     void mocked_tokenservice_returns_right_username() {
@@ -64,50 +68,27 @@ class TransactionServiceTest {
         assertThat(actualList.get(0).getAssetName()).isEqualTo("a");
     }
 
-//    @Test
-//    void createNewTransaction() {
-//        var transactionTest = new TransactionService(mockRootRepository, mockTokenService, mockMailSenderService, mockGenerateMailContent, mailSenderFacade);
-//        //Transaction actualTransaction = transactionTest.createNewTransaction(1,2,2,0.3, "BTC", "ETH");
-//        //assertThat(actualTransaction.getTransactionLog()).isEqualTo(new TransactionLog(2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1));
-//    }
-
     @Test
-    void calculateTransactionCost() {
+    void valid_token_check_returns_filled_List() {
+        var transactionTest = new TransactionService(mockRootRepository, mockTokenService, mockMailSenderFacade);
+        List<TransactionHTMLClient> actualList = transactionTest.authorizeAndGetAssets("123");
+        assertThat(actualList.get(0)).isEqualTo(testTransActionClient);
+
     }
 
     @Test
-    void getMostRecentBuyOrSell() {
+    void method_returns_NotNull() {
+        var transactionTest = new TransactionService(mockRootRepository, mockTokenService, mockMailSenderFacade);
+        Transaction actualTransaction = transactionTest.createNewTransaction(new TransactionData(1, 2, 2.0, "BTC", "ETH", 30000.0, "huibEnNiek"));
+        assertThat(actualTransaction).isNotNull();
     }
 
     @Test
-    void getTransactionHistory() {
-    }
-
-    @Test
-    void determineBuyOrSell() {
-    }
-
-    @Test
-    void getMostRecentTrade() {
-    }
-
-    @Test
-    void deleteTransaction() {
-    }
-
-    @Test
-    void updateAdjustmentFactor() {
-    }
-
-    @Test
-    void setTransaction() {
-    }
-
-    @Test
-    void controlValueAsset() {
-    }
-
-    @Test
-    void controlREcoursesAndExecute() {
+    void method_returns_Transaction_with_checked_value() {
+        var transactionTest = new TransactionService(mockRootRepository, mockTokenService, mockMailSenderFacade);
+        TransactionData actualData = new TransactionData(1, 2, 2.0, "BTC", "ETH", 30000.0, "huibEnNiek");
+        actualData.setTransactionCost(1);
+        Transaction actualTransaction = transactionTest.createNewTransaction(actualData);
+        assertThat(actualTransaction.getTransactionLog()).isEqualTo(expectedLog);
     }
 }
