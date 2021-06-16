7f49877659a2ecf37a25540028ba1149f17e8630
Anna Sophie Koppelaar <annasophiekoppelaar@hotmail.com>
2021-06-16 14:48:54 +0200

    Testen

--- src/test/java/com/example/cryptobank/controller/PortfolioControllerTest.java
+++ src/test/java/com/example/cryptobank/controller/PortfolioControllerTest.java
@@ -3,20 +3,16 @@ package com.example.cryptobank.controller;
 import com.example.cryptobank.domain.asset.Asset;
 import com.example.cryptobank.service.assetenportfolio.PortfolioService;
 import org.junit.jupiter.api.*;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
 import java.util.Arrays;
-
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
