15eaae1b850e8c66e210c0725b4c717e4e7fc335
Anna Sophie Koppelaar <annasophiekoppelaar@hotmail.com>
2021-06-16 14:48:15 +0200

    Testen

--- src/test/java/com/example/cryptobank/controller/PortfolioControllerTest.java
+++ src/test/java/com/example/cryptobank/controller/PortfolioControllerTest.java
@@ -8,19 +8,18 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
-import java.util.List;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringbootTest
+@SpringBootTest
 @AutoConfigureMockMvc
 public class PortfolioControllerTest {
 
@@ -32,36 +31,37 @@ public class PortfolioControllerTest {
 
     @Autowired
     public PortfolioControllerTest(MockMvc mockMvc) {
+        super();
         this.mockMvc = mockMvc;
     }
 
     @Test
     public void portfolio_overview() {
-//        Asset asset = new Asset("Tron", "Tron", "TRX",
-//                "Aims to build a free digital entertainment system",
-//                0.076064, 1.0, 1.0, 1.0, 1.0);
-//
-//        try {
-//        Mockito
-//                .when(portfolioService.showAssetOverview(5))
-//                .thenReturn(Arrays.asList(asset));
-//
-//            MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/portfoliooverview");
-//            request.param("userId", "5");
-//
-//            ResultActions response = mockMvc.perform(request);
-//            response.andExpect(status().isOk());
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+        Asset asset = new Asset("Tron", "Tron", "TRX",
+                "Aims to build a free digital entertainment system",
+                0.076064, 1.0, 1.0, 1.0, 1.0);
+
+        try {
+            Mockito
+                    .when(portfolioService.showAssetOverview(5))
+                    .thenReturn(Arrays.asList(asset));
+
+            MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/portfoliooverview");
+            request.param("userId", "5");
+
+            ResultActions response = mockMvc.perform(request);
+            response.andExpect(status().isOk());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void portfolio_value() {
         try {
-//        Mockito
-//                .when(portfolioService.showValueOfPortfolio(5))
-//                .thenReturn(6321);
+            Mockito
+                    .when(portfolioService.showValueOfPortfolio(5))
+                    .thenReturn(String.valueOf(6321));
 
             MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/portfoliovalue");
             request.param("userId", "5");
