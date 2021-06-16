5f1a0b914dbf3d6fd298028bf2c3804920ba4267
Anna Sophie Koppelaar <annasophiekoppelaar@hotmail.com>
2021-06-16 16:55:58 +0200

    Naam homescherm gewijzigd + url toegevoegd

---
+++ src/main/java/com/example/cryptobank/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.cryptobank.controller;
+
+import com.example.cryptobank.domain.urls.UrlAdresses;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+@RestController
+@RequestMapping
+public class HomeController {
+
+    private UrlAdresses urlAdresses = new UrlAdresses();
+
+    @GetMapping
+    public RedirectView showResetPage() {
+        return new RedirectView(urlAdresses.getHomeschermPage());
+    }
+
+}
--- src/main/java/com/example/cryptobank/controller/TransactionController.java
+++ src/main/java/com/example/cryptobank/controller/TransactionController.java
@@ -100,5 +100,8 @@ public class TransactionController {
         transactionService.setTransaction(transactionData);
     }
 
-    //getUserId > Username
+    @GetMapping("/userid")
+    public long getUserIdByUsername(@RequestHeader(value = "Authorization") String token) {
+        return userService.getUserFromToken(token).getId();
+    }
 }
