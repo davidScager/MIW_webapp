e26220017c7386ef40b322e02ccc984bd9ee9b37
HvS <huibvanstraten@gmail.com>
2021-06-18 07:11:23 +0200

    geupdatet en resourcestream geimplementeerd

--- src/main/java/com/example/cryptobank/service/mailSender/GenerateMailContent.java
+++ src/main/java/com/example/cryptobank/service/mailSender/GenerateMailContent.java
@@ -3,34 +3,34 @@ package com.example.cryptobank.service.mailSender;
 import com.example.cryptobank.domain.maildata.MailData;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 @Component
 public class GenerateMailContent {
     private String mailContent;
-    private Scanner fileReader;
+    private InputStream inputStream;
     private final StringBuilder stringBuilder;
 
     public GenerateMailContent()  {
         this.stringBuilder = new StringBuilder();
     }
 
-    public String setHtmlMail(MailData mailData, String resetLink) throws FileNotFoundException {
-        this.fileReader = new Scanner(new File(mailData.getPageUrl()));
-        readHtmlFile();
-        String correctText = mailContent.replace("TEKSTHIER", mailData.getMailText());
-        return correctText.replace("URLHIER", resetLink);
-    }
-
-    private void readHtmlFile() {
-        while (fileReader.hasNextLine()) {
-            stringBuilder.append(fileReader.nextLine());
+    public String setHtmlMail(MailData mailData, String resetLink) {
+            try {
+                inputStream = getClass().getResourceAsStream ("/default_mail.html");
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                mailContent = reader.lines()
+                        .collect(Collectors.joining(System.lineSeparator()));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+//        readHtmlFile();
+            String correctText = mailContent.replace("TEKSTHIER", mailData.getMailText());
+            return correctText.replace("URLHIER", resetLink);
         }
-        mailContent = String.valueOf(stringBuilder);
-    }
 }
 
 
