5d6f4e2f4722aeb4120fa0aa552362b51b9c1800
HvS <huibvanstraten@gmail.com>
2021-06-18 07:02:04 +0200

    streamReader geimplementeerd

--- src/main/java/com/example/cryptobank/service/mailSender/GenerateMailContent.java
+++ src/main/java/com/example/cryptobank/service/mailSender/GenerateMailContent.java
@@ -3,34 +3,41 @@ package com.example.cryptobank.service.mailSender;
 import com.example.cryptobank.domain.maildata.MailData;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 @Component
 public class GenerateMailContent {
     private String mailContent;
-    private Scanner fileReader;
+    private FileReader fileReader;
     private final StringBuilder stringBuilder;
 
     public GenerateMailContent()  {
         this.stringBuilder = new StringBuilder();
     }
 
-    public String setHtmlMail(MailData mailData, String resetLink) throws FileNotFoundException {
-        this.fileReader = new Scanner(new File(mailData.getPageUrl()));
-        readHtmlFile();
+    public String setHtmlMail(MailData mailData, String resetLink) {
+        try {
+            fileReader = new FileReader("src/main/resources/static/default_mail.html");
+            BufferedReader reader = new BufferedReader(fileReader);
+            mailContent = reader.lines()
+                    .collect(Collectors.joining(System.lineSeparator()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//        readHtmlFile();
         String correctText = mailContent.replace("TEKSTHIER", mailData.getMailText());
         return correctText.replace("URLHIER", resetLink);
     }
 
-    private void readHtmlFile() {
-        while (fileReader.hasNextLine()) {
-            stringBuilder.append(fileReader.nextLine());
-        }
-        mailContent = String.valueOf(stringBuilder);
-    }
+//        private void readHtmlFile() {
+//        while (fileReader.hasNextLine()) {
+//            stringBuilder.append(fileReader.nextLine());
+//        }
+//        mailContent = String.valueOf(stringBuilder);
+//    }
 }
 
 
