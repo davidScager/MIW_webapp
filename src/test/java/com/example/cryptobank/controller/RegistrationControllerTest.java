36aa4e691a5174b1840948770336f5bc4f7dbd2b
David Scager <d.scager@gmail.com>
2021-06-19 00:19:32 +0200

    wrote registrationController tests

--- src/test/java/com/example/cryptobank/controller/RegistrationControllerTest.java
+++ src/test/java/com/example/cryptobank/controller/RegistrationControllerTest.java
@@ -2,63 +2,179 @@ package com.example.cryptobank.controller;
 
 import com.example.cryptobank.domain.login.UserLoginAccount;
 import com.example.cryptobank.domain.urls.UrlAdresses;
-import com.example.cryptobank.domain.user.FullName;
-import com.example.cryptobank.domain.user.Role;
-import com.example.cryptobank.domain.user.User;
-import com.example.cryptobank.domain.user.UserAddress;
 import com.example.cryptobank.service.login.RegistrationService;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.*;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.*;
-import static org.assertj.core.api.Assertions.assertThat;
+
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class RegistrationControllerTest {
+    private final UrlAdresses urlAdresses = new UrlAdresses();
+    private static final String TOKEN = "notoken";
+    private static final String JWT_SUBJECT = "Register";
+    private static final String JSON_BODY =
+            "{\"user\":{" +
+                "\"bsn\":\"123456\"," +
+                "\"fullName\": {" +
+                    "\"firstName\":\"Dingetje\"," +
+                    "\"infix\":\"van\"," +
+                    "\"surname\":\"Dinges\"}," +
+                "\"dateOfBirth\":\"2000-01-01\"," +
+                "\"userAddress\":{" +
+                    "\"postalCode\":\"2345AB\"," +
+                    "\"houseNr\":\"20\"," +
+                    "\"addition\":\"\"," +
+                    "\"streetName\":\"Dorpstraat\"," +
+                    "\"residence\":\"Lutjebroek\"}," +
+                "\"email\":\"not.an@email.org\"}," +
+            "\"password\":\"password\"" +
+            "}";
 
     @Mock
     private RegistrationService mockRegistrationService;
 
-    @Mock
-    private UserLoginAccount mockUserLoginAccount;
-
     @BeforeEach
     public void initRestAssuredMockMvcStandAlone(){
-        this.mockRegistrationService = Mockito.mock(RegistrationService.class);
-        this.mockUserLoginAccount = Mockito.mock(UserLoginAccount.class);
+        mockRegistrationService = Mockito.mock(RegistrationService.class);
         RestAssuredMockMvc.standaloneSetup(new RegistrationController(mockRegistrationService));
     }
 
-    // requirement registering client
-    // 0- Body: application/json format
-    // 1- URL: POST "/registerclient"
-    // 2- status code 200 OK
-    // 3- application/json body format in response
     @Test @Order(1)
-    public void registrationRequest_returns_200_OK(){
-        Mockito.when(mockRegistrationService.validate(Mockito.any(UserLoginAccount.class))).thenReturn(true);
-
-        given()
-                .contentType("application/json")
-        .when()
-                .post("/register/client")
+    public void viewHtmlRegisterHandler_returns_302_moved_temporarily(){
+        when()
+                .get("/register")
         .then()
-                .statusCode(200);
+                .statusCode(302);
     }
 
     @Test @Order(2)
-    public void registrationRequest_no_request_body_returns_400(){
+    public void viewHtmlRegisterHandler_header_contains_right_url(){
+        String registerUrl = urlAdresses.getRegistrationPageUrl();
+
+        when()
+                .get("/register")
+        .then()
+                .header("Location", registerUrl);
+    }
+
+    @Test @Order(3)
+    public void viewHtmlRegisterFailedHandler_returns_302_moved_temporarily(){
+        when()
+                .get("/register/failed")
+        .then()
+                .statusCode(302);
+    }
+
+    @Test @Order(4)
+    public void viewHtmlRegisterFailedHandler_header_contains_right_url(){
+        String registerFailedUrl = urlAdresses.getRegistrationFailedPageUrl();
+
+        when()
+                .get("/register/failed")
+        .then()
+                .header("Location", registerFailedUrl);
+    }
+
+    @Test @Order(5)
+    public void register_request_given_json_body_returns_200(){
+        Mockito.when(mockRegistrationService.validate(Mockito.any(UserLoginAccount.class))).thenReturn(true);
+        Mockito.when(mockRegistrationService.cacheNewUserWithToken(Mockito.any(UserLoginAccount.class))).thenReturn(TOKEN);
+        Mockito.when(mockRegistrationService.confirmationEmailSent(Mockito.anyString(), Mockito.anyString())).thenReturn(true);
+
         given()
-                .body("")
-                .contentType("application/json")
+                .body(JSON_BODY)
+                .contentType(ContentType.JSON)
         .when()
-                .post("/register/client")
+                .post("/register/request")
+        .then()
+                .statusCode(200);
+
+    }
+
+    @Test @Order(6)
+    public void register_request_given_json_body_email_fails_returns_400(){
+        Mockito.when(mockRegistrationService.validate(Mockito.any(UserLoginAccount.class))).thenReturn(true);
+        Mockito.when(mockRegistrationService.cacheNewUserWithToken(Mockito.any(UserLoginAccount.class))).thenReturn(TOKEN);
+        Mockito.when(mockRegistrationService.confirmationEmailSent(Mockito.anyString(), Mockito.anyString())).thenReturn(false);
+
+        given()
+                .body(JSON_BODY)
+                .contentType(ContentType.JSON)
+        .when()
+                .post("/register/request")
         .then()
                 .statusCode(400);
+
     }
+
+    @Test @Order(7)
+    public void register_request_given_json_body_user_already_registered_returns_400(){
+        Mockito.when(mockRegistrationService.validate(Mockito.any(UserLoginAccount.class))).thenReturn(false);
+
+        given()
+                .body(JSON_BODY)
+                .contentType(ContentType.JSON)
+        .when()
+                .post("/register/request")
+        .then()
+                .statusCode(400);
+
+    }
+
+    @Test @Order(8)
+    public void register_finalize_given_valid_token_param_returns_302(){
+        Mockito.when(mockRegistrationService.validateToken(TOKEN, JWT_SUBJECT)).thenReturn(true);
+
+        given()
+                .param("Authorization", TOKEN)
+        .when()
+                .get("/register/finalize")
+        .then()
+                .statusCode(302);
+    }
+
+    @Test @Order(9)
+    public void register_finalize_given_valid_token_param_header_contains_right_url(){
+        String loginRedirect = urlAdresses.getLoginPage();
+        Mockito.when(mockRegistrationService.validateToken(TOKEN, JWT_SUBJECT)).thenReturn(true);
+
+        given()
+                .param("Authorization", TOKEN)
+        .when()
+                .get("/register/finalize")
+        .then()
+                .header("Location", loginRedirect);
+    }
+
+    @Test @Order(10)
+    public void register_finalize_given_invalid_token_param_returns_302(){
+        Mockito.when(mockRegistrationService.validateToken(TOKEN, JWT_SUBJECT)).thenReturn(false);
+
+        given()
+                .param("Authorization", TOKEN)
+        .when()
+                .get("/register/finalize")
+        .then()
+                .statusCode(302);
+    }
+
+    @Test @Order(11)
+    public void register_finalize_given_invalid_token_param_header_contains_right_url(){
+        String registrationFailedUrl = urlAdresses.getRegistrationFailedPageUrl();
+        Mockito.when(mockRegistrationService.validateToken(TOKEN, JWT_SUBJECT)).thenReturn(false);
+
+        given()
+                .param("Authorization", TOKEN)
+        .when()
+                .get("/register/finalize")
+        .then()
+                .header("Location", registrationFailedUrl);
+    }
+
 }
